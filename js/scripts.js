92880f541a6db401fed2e71ba60adbf33c7be9bf
RedouanAF <redouan1af@gmail.com>
2019-12-12 18:52:42 +0100

    done

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,41 @@
-
-
 let cross = document.querySelectorAll('.show-more--icon');
 let list_card = document.querySelectorAll('.main-content__steps-card-list');
 
 cross.forEach((crs, index) => {
     crs.addEventListener('click', (e) => {
-        if((e.target.style.transform == 'rotate(0deg)')){
+        if ((e.target.style.transform == 'rotate(0deg)')) {
             e.target.style.transform = 'rotate(45deg)';
             list_card[index].classList.toggle('list-hidden');
-        }
-        else{
-            e.target.style.transform =  'rotate(0deg)';
+        } else {
+            e.target.style.transform = 'rotate(0deg)';
             list_card[index].classList.toggle('list-hidden');
-        } 
+        }
     });
 });
 
-const navSlide = () =>{
-    const burger = document.querySelector('.header__wrapper-nav--burger');
-    const nav = document.querySelector('.header__wrapper-nav');
-    var burger_img = document.querySelector('.burger-img');
+//menu switching
+var burger_img = document.querySelector('.burger-img');
+function Toggle() {
+    let initialImg = document.querySelector(".burger-img").src;
+    let srcTest = initialImg.includes('img/burger.svg');
+    let newImg = {
+        'true': 'img/close.svg',
+        'false': 'img/burger.svg'
+    } [srcTest];
+    return newImg;
+}
+var navSlide = () => {
+    var burger = document.querySelector('.header__wrapper-nav--burger');
+    var nav = document.querySelector('.header__wrapper-nav');
     var main = document.querySelector('.main');
     var footer = document.querySelector('.footer');
-    burger_img.addEventListener('click', (e)=>{
-        
-        if(burger_img.src === "img/groupe-84.svg"){
-            //e.preventDefault();
-            burger_img.src = "img/burger.svg";
-            //nav.classList.toggle('header__wrapper-nav-active');
-            main.classList.toggle('block');
-            footer.classList.toggle('block');
-            
-        }else{
-            
-            burger_img.src = "img/groupe-84.svg";
-            nav.classList.toggle('header__wrapper-nav-active');
-            main.classList.toggle('block');
-            footer.classList.toggle('block');
-        }
-        
+
+    burger_img.addEventListener('click', () => {
+    main.classList.toggle('block');
+    footer.classList.toggle('block');
+    initialImg = document.querySelector(".burger-img").src = Toggle();
+    nav.classList.toggle('header__wrapper-nav-active');
     })
 };
-
 
 navSlide();
